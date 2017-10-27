39830992a28f715eec9c75d398155ee914dcf3c5
ilhamnovitra <ilham15ti@mahasiswa.pcr.ac.id>
2017-10-27 10:22:30 +0700

    tes

--- MIN3TIC_GITHUB/Github_Pertama/CobaGithub.aspx.vb
+++ MIN3TIC_GITHUB/Github_Pertama/CobaGithub.aspx.vb
@@ -2,7 +2,7 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        Label1.Text = "Selamat Datang di Github"
+        Label1.Text = "Selamat Datang di Gasdithub"
     End Sub
 
 End Class
